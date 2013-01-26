cc80cf91f5b7a717d6fa0a33c2b1cc5fc3077f8e
codeguru <codeguru@users.sourceforge.net>
2013-01-26 13:48:26 -0700

    Create Android project in android subdirectory

---
+++ android/src/codeguru/surface3d/Surface3DMain.java
@@ -0,0 +1,15 @@
+package codeguru.surface3d;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Surface3DMain extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
