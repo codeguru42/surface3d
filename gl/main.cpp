64c8b7f7d83ce251e6b5df85a5b5b1102aa3eb97
codeguru <codeguru@users.sourceforge.net>
2013-11-03 13:10:17 -0700

    GL: Render a sample surface

--- gl/main.cpp
+++ gl/main.cpp
@@ -1,7 +1,9 @@
 #include <GL/freeglut.h>
+#include <cmath>
 
-#define MIN -1.0
-#define MAX  1.0
+#define MIN -25.0
+#define MAX  25.0
+#define DELTA 0.5
 
 GLfloat lerp(GLfloat x,
              GLfloat xMin, GLfloat xMax,
@@ -9,16 +11,33 @@ GLfloat lerp(GLfloat x,
   return x * (yMax - yMin) / (xMax - xMin);
 }
 
+GLfloat func(GLfloat x, GLfloat y) {
+  GLfloat radical = std::sqrt(x * x + y * y);
+  return 4 * std::sin(radical) / radical;
+}
+
 void display() {
   glClear(GL_COLOR_BUFFER_BIT);
 
   glColor3f(1.0, 1.0, 1.0);
-  glBegin(GL_POLYGON); {
-    glVertex3f( 0.5,  0.5, 0.0);
-    glVertex3f(-0.5,  0.5, 0.0);
-    glVertex3f(-0.5, -0.5, 0.0);
-    glVertex3f( 0.5, -0.5, 0.0);
-  } glEnd();
+
+  for (GLfloat x = MIN; x < MAX; x += DELTA) {
+    glBegin(GL_LINE_STRIP);
+    for (GLfloat y = MIN; y < MAX; y += DELTA) {
+      GLfloat z = func(x, y);
+      glVertex3f(x, y, z);
+    }
+    glEnd();
+  }
+
+  for (GLfloat y = MIN; y < MAX; y += DELTA) {
+    glBegin(GL_LINE_STRIP);
+    for (GLfloat x = MIN; x < MAX; x += DELTA) {
+      GLfloat z = func(x, y);
+      glVertex3f(x, y, z);
+    }
+    glEnd();
+  }
 
   glFlush();
 }
@@ -32,20 +51,27 @@ void reshape(int w, int h) {
   glViewport (0, 0, (GLsizei) w, (GLsizei) h);
   glMatrixMode (GL_PROJECTION);
   glLoadIdentity();
-  if (w <= h)
-    glOrtho (MIN, MAX, lerp(MIN, 0, w, 0, h),
-      lerp(MAX, 0, w, 0, h), -10.0, 10.0);
-  else
-    glOrtho (lerp(MIN, 0, h, 0, w),
-      lerp(MAX, 0, h, 0, w), MIN, MAX, -10.0, 10.0);
+
+  if (w <= h) {
+    glOrtho(MIN, MAX,
+            lerp(MIN, 0, w, 0, h), lerp(MAX, 0, w, 0, h),
+            2*MIN, 2*MAX);
+  } else {
+    glOrtho(lerp(MIN, 0, h, 0, w), lerp(MAX, 0, h, 0, w),
+            MIN, MAX,
+            2*MIN, 2*MAX);
+  }
+
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
+  glRotated(-70.0, 1.0, 0.0, 0.0);
+  glRotated( 30.0, 0.0, 0.0, 1.0);
 }
 
 int main(int argc, char** argv) {
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-  glutInitWindowSize(250, 250);
+  glutInitWindowSize(800, 800);
   glutInitWindowPosition(100, 100);
   glutCreateWindow("hello");
   init();
